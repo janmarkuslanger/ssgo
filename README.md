e4fcb7a70055de5203a10d3ee3b62710830eeea1
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-07-27 21:47:35 +0200

    feat: add extract pattern

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 <p align="center"><img src="/logo.svg" alt="Logo" width="200" /></p>
 <h1 align="center">SSGO</h1>
-
----
--- page/pattern_test.go
+++ page/pattern_test.go
@@ -1,7 +1,19 @@
 package page_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/janmarkuslanger/ssgo/page"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestExtractPattern(t *testing.T) {
-	ass
+	assert.Equal(t, page.ExtractPattern("/hello/:id", "/hello/123"), map[string]string{
+		"id": "123",
+	})
+
+	assert.Equal(t, page.ExtractPattern("/:foo/:id", "/hello/123"), map[string]string{
+		"foo": "hello",
+		"id":  "123",
+	})
 }
