2730e3eea632b5fd75d0e6949ef0d49263cfd162
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-07-30 18:36:40 +0200

    add build func for builder

--- builder/builder.go
+++ builder/builder.go
@@ -1,6 +1,9 @@
 package builder
 
 import (
+	"fmt"
+	"path/filepath"
+
 	"github.com/janmarkuslanger/ssgo/page"
 	"github.com/janmarkuslanger/ssgo/rendering"
 	"github.com/janmarkuslanger/ssgo/writer"
@@ -11,4 +14,28 @@ type Builder struct {
 	Pages     []page.Generator
 	Writer    writer.Writer
 	Renderer  rendering.Renderer
+}
+
+func (b Builder) Build() error {
+	for _, g := range b.Pages {
+		pages, err := g.GeneratePageInstances()
+		if err != nil {
+			return fmt.Errorf("failed to generate pages: %w", err)
+		}
+
+		for _, p := range pages {
+			content, err := p.Render()
+			if err != nil {
+				// TODO: make configurable if it should continue if single page faileds
+				return fmt.Errorf("failed to render page %s: %w", p.Path, err)
+			}
+
+			fullPath := filepath.Join(b.OutputDir, p.Path)
+			if err := b.Writer.Write(fullPath, content); err != nil {
+				return fmt.Errorf("failed to write page %s: %w", p.Path, err)
+			}
+		}
+	}
+
+	return nil
 }
---
+++ builder/builder_test.go
@@ -0,0 +1,126 @@
+package builder_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/janmarkuslanger/ssgo/builder"
+	"github.com/janmarkuslanger/ssgo/page"
+	"github.com/janmarkuslanger/ssgo/rendering"
+)
+
+type MockWriter struct{}
+
+func (w MockWriter) Write(filepath string, content string) (err error) {
+	return nil
+}
+
+type MockWriterFail struct{}
+
+func (w MockWriterFail) Write(filepath string, content string) (err error) {
+	return errors.New("something went wrong")
+}
+
+type MockRenderer struct{}
+
+func (r MockRenderer) Render(ctx rendering.RenderContext) (output string, err error) {
+	return "hello world", nil
+}
+
+type MockRendererFail struct{}
+
+func (r MockRendererFail) Render(ctx rendering.RenderContext) (output string, err error) {
+	return "", errors.New("something went wrong")
+}
+
+func TestBuilder_Build_success(t *testing.T) {
+	b := builder.Builder{
+		OutputDir: "/test",
+		Writer:    MockWriter{},
+		Pages: []page.Generator{
+			{
+				Config: page.Config{
+					Renderer: MockRenderer{},
+					GetPaths: func() []string {
+						return []string{"a", "b"}
+					},
+				},
+			},
+		},
+	}
+
+	err := b.Build()
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestBuilder_Build_failinggenerators(t *testing.T) {
+	b := builder.Builder{
+		OutputDir: "/test",
+		Writer:    MockWriter{},
+		Pages: []page.Generator{
+			{
+				Config: page.Config{
+					Renderer: nil,
+					GetPaths: func() []string {
+						return []string{"a", "b"}
+					},
+				},
+			},
+		},
+	}
+
+	err := b.Build()
+
+	if err == nil {
+		t.Fatal("expected an error but got nil")
+	}
+}
+
+func TestBuilder_Build_failingwriter(t *testing.T) {
+	b := builder.Builder{
+		OutputDir: "/test",
+		Writer:    MockWriterFail{},
+		Pages: []page.Generator{
+			{
+				Config: page.Config{
+					Renderer: MockRenderer{},
+					GetPaths: func() []string {
+						return []string{"a", "b"}
+					},
+				},
+			},
+		},
+	}
+
+	err := b.Build()
+
+	if err == nil {
+		t.Fatal("expected an error but got nil")
+	}
+}
+
+func TestBuilder_Build_failingrenderer(t *testing.T) {
+	b := builder.Builder{
+		OutputDir: "/test",
+		Writer:    MockWriterFail{},
+		Pages: []page.Generator{
+			{
+				Config: page.Config{
+					Renderer: MockRendererFail{},
+					GetPaths: func() []string {
+						return []string{"a", "b"}
+					},
+				},
+			},
+		},
+	}
+
+	err := b.Build()
+
+	if err == nil {
+		t.Fatal("expected an error but got nil")
+	}
+}
